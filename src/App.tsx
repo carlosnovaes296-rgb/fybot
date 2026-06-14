f36c52f6a2de4a153cee1157d63740bf11ee158f
webmaine <jfcn2020@gmail.com>
2026-06-14 12:17:07 -0300

    UI: ajustar placa fybot mais para a direita

--- src/App.tsx
+++ src/App.tsx
@@ -1007,7 +1007,7 @@ export default function App() {
           <img
             src="/fybot_neon_dubai.png.png"
             alt="Futuristic Robot Background"
-            className="w-full h-full object-cover object-center select-none pointer-events-none transition-all duration-1000 ease-out opacity-100"
+            className="w-full h-full object-cover object-[20%_center] select-none pointer-events-none transition-all duration-1000 ease-out opacity-100"
             referrerPolicy="no-referrer"
           />
         </div>
